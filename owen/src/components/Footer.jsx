359044c4331653e8e83e09205409907aa66ac0e4
samdroste <sdroste@x-cago.com>
2025-11-04 12:07:34 +0100

    asdsa
    dsada

--- owen/src/components/Footer.jsx
+++ owen/src/components/Footer.jsx
@@ -5,29 +5,23 @@ const Footer = () => {
   return (
     <footer className="bg-bg text-gray-900 w-full border-t border-gray-200 md:fixed md:bottom-0 md:left-0">
       <div className="max-w-7xl mx-auto px-6 py-4 text-center text-sm md:text-base space-y-2">
-        {/* Links naar Privacy en Algemene Voorwaarden */}
+        {/* Links */}
         <div className="flex justify-center space-x-4 mb-1">
-          <Link
-            to="/privacy"
-            className="text-blue-700 hover:underline text-sm md:text-base"
-          >
+          <Link to="/privacy" className="text-blue-700 hover:text-blue-800 hover:underline text-sm md:text-base">
             Privacybeleid
           </Link>
-          <Link
-            to="/tos"
-            className="text-blue-700 hover:underline text-sm md:text-base"
-          >
+          <Link to="/tos" className="text-blue-700 hover:text-blue-800 hover:underline text-sm md:text-base">
             Algemene Voorwaarden
           </Link>
         </div>
 
         {/* Copyright */}
-        <div className="text-gray-500 text-sm md:text-base">
+        <div className="text-gray-800 text-sm md:text-base">
           © {new Date().getFullYear()} <span className="font-semibold">Collab4You</span>. Alle rechten voorbehouden.
         </div>
 
-        {/* Disclaimer onderaan */}
-        <div className="text-gray-600 text-xs mt-1 md:mt-2">
+        {/* Disclaimer */}
+        <div className="text-gray-700 text-xs mt-1 md:mt-2">
           De informatie op deze website is uitsluitend bedoeld voor algemene informatie. 
           Hoewel we streven naar correcte informatie, kan <span className="font-semibold">Collab4You</span> niet aansprakelijk worden gesteld voor fouten of onvolledigheden.
         </div>
